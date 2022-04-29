86bf2455be4f52630f59e096678864f759235989
ClaireSprunger <cksprung@asu.edu>
2022-04-28 20:43:28 -0700

    help ahhh

---
+++ program-book/js/scripts.js
@@ -0,0 +1,48 @@
+var slider = tns({
+    container: ".my-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
+  var slider = tns({
+    container: ".mobile-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
+  var slider = tns({
+    container: ".motion-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
+  var slider = tns({
+    container: ".process-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
