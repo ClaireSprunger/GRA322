291bfc45166fe29abc5ddddc4372f41f736f3f86
ClaireSprunger <cksprung@asu.edu>
2022-04-30 18:12:01 -0700

    heyo

--- program-book/js/scripts.js
+++ program-book/js/scripts.js
@@ -11,6 +11,18 @@ var slider = tns({
   });
 
   var slider = tns({
+    container: ".motion-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
+  var slider = tns({
     container: ".mobile-slider",
     items: 1,
     slideBy: "page",
@@ -24,6 +36,19 @@ var slider = tns({
 
   var slider = tns({
     container: ".progress-slider",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    prevButton:false,
+    nextButton: false,
+    controls: false,
+    nav: false,
+  });
+
+
+  var slider = tns({
+    container: ".app-slider",
     items: 1,
     slideBy: "page",
     autoplay: true,
